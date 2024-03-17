a470cc5a2dbcb16aac3b0c78d62a66b0e5feb53d
achojnacka <anna.chojnacka5.90@gmail.com>
2024-03-17 19:44:23 +0100

    hello_world

---
+++ hello_world2/test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using hello_world;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+
+        }
+    }
